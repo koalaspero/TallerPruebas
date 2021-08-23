c08919aec7eba43f98291881bbef9c201474f3ae
sjgarcia3098 <74066130+sjgarcia3098@users.noreply.github.com>
2021-08-23 13:50:37 -0500

    Comentarios en SupervisorTest y ManagerTest

--- test/ManagerTest.java
+++ test/ManagerTest.java
@@ -11,25 +11,34 @@ import tallerPruebas.EmployeeType;
 
 public class ManagerTest {
     Employee manager;
-
+	
+    /*En este caso de prueba, se verifica que haya una salida correcta del metodo cs()  
+        al utlizar una instancia de Empleado con enum Manager y con USD como su moneda*/
     @Test
     public void csManager1Test() {//Caso de prueba en el que se calcula 
         manager = new Employee(20, "USD", 0, EmployeeType.Manager);
         assertNotEquals(0.0f,manager.cs());
     }
 
+    /*En este caso de prueba, se verifica que haya una salida correcta del metodo CalculateYearBonus()
+    	al utlizar una instancia de Empleado con enum Manager y con USD como su moneda*/
     @Test
     public void cybManager1Test() {
         manager = new Employee(20, "USD", 0, EmployeeType.Manager);
         assertNotEquals(0.0f,manager.CalculateYearBonus());
     }
 
+    /*En este caso de prueba, se verifica que haya una salida correcta del metodo cs()  
+        al utlizar una instancia de Empleado con enum Manager y con ERU como su moneda*/
     @Test
     public void csManager2Test () {
         manager = new Employee(20, "EUR", 0, EmployeeType.Manager);
         assertNotEquals(0.0f,manager.cs());
 
     }
+
+    /*En este caso de prueba, se verifica que haya una salida correcta del metodo CalculateYearBonus()
+        al utlizar una instancia de Empleado con enum Manager y con EUR como su moneda*/
     @Test
     public void cybManager2Test() {
         manager = new Employee(20, "EUR", 0, EmployeeType.Manager);
--- test/SupervisorTest.java
+++ test/SupervisorTest.java
@@ -17,25 +17,33 @@ public class SupervisorTest {
         supervisor1 = new Employee(20, "USD", 0, EmployeeType.Supervisor);
         supervisor2 = new Employee(20, "EUR", 0, EmployeeType.Supervisor);
     }
-        
+     
+    /*En este caso de prueba, se verifica que haya una salida correcta del metodo CalculateYearBonus()
+    	al utlizar una instancia de Empleado con enum Supervisor y con USD como su moneda*/   	   
     @Test
     public void cybSupervisor1() {
         float bonusSupervisor = supervisor1.CalculateYearBonus();
         assertNotEquals(0.0F, bonusSupervisor);
     }
     
+    /*En este caso de prueba, se verifica que haya una salida correcta del metodo cs()  
+        al utlizar una instancia de Empleado con enum Supervisor y con USD como su moneda*/
     @Test
     public void csSupervisor1() {
     	float SalarySupervisor= supervisor1.cs();
         assertNotEquals(0.0F, SalarySupervisor);
     }
     
+    /*En este caso de prueba, se verifica que haya una salida correcta del metodo CalculateYearBonus()
+        al utlizar una instancia de Empleado con enum Supervisor y con EUR como su moneda*/
     @Test
     public void cybSupervisor2() {
         float bonusSupervisor = supervisor2.CalculateYearBonus();
         assertNotEquals(0.0F, bonusSupervisor);
     }
     
+    /*En este caso de prueba, se verifica que haya una salida correcta del metodo cs()  
+        al utlizar una instancia de Empleado con enum Supervisor y con ERU como su moneda*/
     @Test
     public void csSupervisor2() {
     	float SalarySupervisor= supervisor2.cs();
